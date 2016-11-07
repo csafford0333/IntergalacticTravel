6e4da3d6682785870be3a755d06d3085663ba43c
student5 <student3@jh-mba158-19.local>
2016-11-07 09:53:11 -0600

    added images

--- StarViewController.swift
+++ StarViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class StarViewController: UIViewController {
-    var blueStar: Bool?
+    @IBOutlet weak var starImageView: UIImageView!
+    
+    var isBlueStar: Bool?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        if isBlueStar!
+        {
+            view.backgroundColor = UIColor.blue
+            starImageView.image = UIImage(named: "blueStar")
+        }
+        else
+        {
+            view.backgroundColor = UIColor.red
+            starImageView.image = UIImage(named: "redStar")
+
+        }
     }
 
     
